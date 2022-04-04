6cffbb95be40d23fc2c22c829dd9c515b28531b2
keyve.njsampaio <keyve.njsampaio@ITQ0626033W10-1>
2022-04-04 11:54:51 -0300

    Adicionar arquivos de projeto.

---
+++ atividade/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace atividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+          int p = 3;
+          int j = 6;
+          int [,] vs = new int [p,j]
+
+        
+         }
+
+           
+        }
+    }
+}
