b20487ca589a2ce0a2f8b548765996bc3f5a072d
Keyve Sampaio <keyve10@live.com>
2022-04-07 02:23:13 -0300

    atualização

--- atividade/Program.cs
+++ atividade/Program.cs
@@ -8,9 +8,42 @@ namespace atividade
         static void Main(string[] args)
 
         {
-          int p = 3;
-          int j = 6;
-          int [,] vs = new int [p,j]
+           Console.WriteLine();
+            Console.WriteLine("Digite a quantidade de cartões");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade de número para cartões");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            int[,] m = new int[c, n];
+            Random random = new Random();
+            for (int i = 0; i < m.Length; i++)
+            {
+                int sorteado = random.Next(1, 60);
+                if (i == 0)
+                    m [c, n] = sorteado;
+                else
+                {
+                    if (Array.BinarySearch(m, sorteado) < 0)
+                        m[c, n] = sorteado;
+                }
+                if (i != m.Length - 1)
+                    Console.WriteLine(m[c, n] + "-");
+                else
+                    Console.WriteLine(m[c, n]);
+            }
+            Console.WriteLine();
+            Array.Sort(m);
+            for (int i = 0; i < m.Length; i++)
+            {
+                if (i != m.Length - 1)
+                    Console.Write(m[c, n]);
+                else
+                    Console.Write(m[c, n]);
+            }
+            Console.ReadKey();
+        
+    
 
         
          }
@@ -18,4 +51,3 @@ namespace atividade
            
         }
     }
-}
